8357c4cb344ea5e9ed1fe1c7e2ceac6e70cc2ea4
lanhin <lanhin1@gmail.com>
2016-01-16 21:45:05 +0800

    Added q2a.py, which runs much faster than shell script.

---
+++ q2a.py
@@ -0,0 +1,42 @@
+#/usr/bin/env python
+'''
+@2016-01  by lanhin
+
+Transform fastq format into fasta format.
+Usage: python2 q2a.py fastqFileName.fq
+
+Fastq format:
+    @readid/direction
+    sequence line
+    +
+    quality line
+
+Fasta format:
+    >readid/direction
+    sequence line
+'''
+import sys
+import os
+
+if (len(sys.argv)) != 2:
+    print "nonon"
+    exit()
+
+fqfilename=sys.argv[1]
+fafilename=fqfilename + '.fa'
+
+print fqfilename, fafilename
+
+if os.path.isfile(fafilename):
+    os.remove(fafilename)
+linenum = 0
+with open (fafilename, 'w') as faOut:
+    with open (fqfilename, 'r') as fqIn:
+        for line in fqIn.readlines():
+            if linenum == 0:
+                line = line.replace('@', '>')
+                #print line
+                faOut.write(line)
+            elif linenum == 1:
+                faOut.write(line)
+            linenum = ( linenum + 1 ) % 4
